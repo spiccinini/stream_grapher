03fa9f8bd9dc3d77f5ed225c04c6a80da4c2cda5
san <san@localhost>
2010-09-26 17:45:09 -0300

    Fix in biompedancia backend

--- backends/bioimpedancia.py
+++ backends/bioimpedancia.py
@@ -1,37 +1,48 @@
 # -*- coding: UTF-8 -*-
 
-import serial
+#import serial
+import random
 from itertools import izip
+from backend import Backend
 
 def grouper(n, iterable):
     "grouper(3, 'ABCDEFG') --> 'ABC' 'DEF'"
     args = [iter(iterable)] * n
     return ["".join(x) for x in izip(*args)]
 
-class Bioimpedancia(object):
+class Bioimpedancia(Backend):
     PACKET_LENGTH = 16
     DATA_LENGTH = PACKET_LENGTH * 4
 
-    def __init__(self, port="COM2", timeout=0.5):
-        self.ser = serial.Serial(port=port, timeout=timeout)
+    def __init__(self, port="COM1", timeout=0.1):
+        Backend.__init__(self, ports=16, sample_rate=10) # Unknown sample_rate
+        #self.ser = serial.Serial(port=port, timeout=timeout)
+        #self.ser.flushInput()
         self._buffer = ""
+        self.f = open("backends/biompedancia_1.log", "r")
 
     def get_remaining_samples(self):
-        self._buffer += self.ser.read(1)
-        self._buffer += self.ser.read(self.ser.inWaiting())
-
-        sobrantes = len(self._buffer) % DATA_LENGTH
-        data = grouper(DATA_LENGTH, self._buffer)
+        import time
+        time.sleep(random.random()/5.)
+        leido = self.f.read(random.randint(1,100))
+        #leido += self.ser.read(self.ser.inWaiting())
+        #self.f.write(leido)
+        self._buffer += leido
+        
+        sobrantes = len(self._buffer) % Bioimpedancia.DATA_LENGTH
+        data = grouper(Bioimpedancia.DATA_LENGTH, self._buffer)
         self._buffer = self._buffer[-sobrantes:]
         samples = [[]]*16 # 16 canales, 4 frecuencias por 4 mediciones.
         for meta_packet in data:
-            packets = grouper(PACKET_LENGTH, meta_packet)
+            packets = grouper(Bioimpedancia.PACKET_LENGTH, meta_packet)
+            print packets
             for i, packet in enumerate(packets):
-                decod = [int(x.encode("hex"),16) for x in packet[:9]]
-                samples[4*i+0].append(decod[1]*256+decod[2])
-                samples[4*i+1].append(decod[3]*256+decod[4])
-                samples[4*i+2].append(decod[5]*256+decod[6])
-                samples[4*i+3].append(decod[7]*256+decod[8])
+                decod = [int(x.encode("hex"),16) for x in packet[1:9]]
+                samples[4*i+0].append(decod[0]*256+decod[1])
+                samples[4*i+1].append(decod[2]*256+decod[3])
+                samples[4*i+2].append(decod[4]*256+decod[5])
+                samples[4*i+3].append(decod[6]*256+decod[7])
+        
         return samples
 
     def close(self):
