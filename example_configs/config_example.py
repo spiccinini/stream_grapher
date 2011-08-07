8eb3f8a05ceabb61122301a8681273de4f3f12e6
Santiago Piccinini (SAn) <piccinini.santiago@gmail.com>
2011-08-07 15:25:46 -0300

    Added FFTGraph to config_example

--- example_configs/config_example.py
+++ example_configs/config_example.py
@@ -3,6 +3,7 @@
 from stream_grapher.widgets.stream_graph import StreamGraph
 from stream_grapher.widgets.multiple_stream_graph import MultipleStreamGraph
 from stream_grapher.widgets.browsable_stream_graph import BrowsableStreamGraph
+from stream_grapher.widgets.fft_graph import FFTGraph
 from stream_grapher.widgets.qt_graph import QGLDrawer, QGLDrawerHScroll
 from stream_grapher.connection import PatchBay
 
@@ -10,21 +11,32 @@ backends = []
 widgets = []
 
 from stream_grapher.backends.sthocastic import Brownian
-sample_generator  = Brownian(ports=4, sample_rate=100)
+sample_generator  = Brownian(ports=3, sample_rate=100)
 
 backends.append(sample_generator)
 
-widget1 = QGLDrawer(StreamGraph(n_samples=300, size=(2, 2), position=(-1, -1), color=(255,205,205)))
-widget2 = QGLDrawer(MultipleStreamGraph(n_graphs=2, n_samples=300, size=(2, 2),
-                                        position=(-1, -1),
-                                        colors=[(135,25,255*i) for i in range(2)]))
-widget3 = QGLDrawerHScroll(BrowsableStreamGraph(n_samples=300, size=(2, 2), position=(-1, -1), color=(255,205,205)))
+simple_widget = QGLDrawer(StreamGraph(n_samples=300, size=(2, 2), position=(-1, -1),
+                                      color=(255,205,205)))
 
-widgets.append(widget1)
-widgets.append(widget2)
-widgets.append(widget3)
+multiple_widget = QGLDrawer(MultipleStreamGraph(n_graphs=2, n_samples=300, size=(2, 2),
+                                                position=(-1, -1),
+                                                colors=[(135,25,255*i) for i in range(2)]))
 
-PatchBay.connect(src=sample_generator, src_port=1, out=widget1)
-PatchBay.connect(src=sample_generator, src_port=2, out_port=1, out=widget2)
-PatchBay.connect(src=sample_generator, src_port=3, out_port=2, out=widget2)
-PatchBay.connect(src=sample_generator, src_port=4, out=widget3)
+scrollable_widget = QGLDrawerHScroll(BrowsableStreamGraph(n_samples=300, size=(2, 2),
+                                                          position=(-1, -1), color=(255,205,205)))
+
+fft_widget = QGLDrawer(FFTGraph(fft_size=1024, fft_window_size=128,
+                                sample_rate=sample_generator.sample_rate,
+                                 size=(2, 2), position=(-1, -1), color=(205,24,57),
+                                 window_type="boxcar"))
+
+widgets.append(simple_widget)
+widgets.append(multiple_widget)
+widgets.append(scrollable_widget)
+widgets.append(fft_widget)
+
+PatchBay.connect(src=sample_generator, src_port=1, out=simple_widget)
+PatchBay.connect(src=sample_generator, src_port=2, out_port=1, out=multiple_widget)
+PatchBay.connect(src=sample_generator, src_port=3, out_port=2, out=multiple_widget)
+PatchBay.connect(src=sample_generator, src_port=1, out=scrollable_widget)
+PatchBay.connect(src=sample_generator, src_port=1, out=fft_widget)
